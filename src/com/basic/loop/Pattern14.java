1a17793d02acd46a30120ac9efd672cecbecce4e
Sandeshcreator <mainalisandesh8@gmail.com>
2025-01-13 21:19:23 +0545

    feat: Add Pattern11, Pattern12, Pattern14, and Pattern20

---
+++ src/com/basic/loop/Pattern14.java
@@ -0,0 +1,10 @@
+package com.basic.loop;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        int input = 7;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(input + "  *  " + i + "  =  " + (input * i));
+        }
+    }
+}
