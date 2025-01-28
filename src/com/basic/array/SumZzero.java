9e45a1c7e801f5ee7d9f6219d0be6aa86bdac575
Sandeshcreator <mainalisandesh8@gmail.com>
2025-01-28 20:02:11 +0545

    public int[] sumZero(int n) {

---
+++ src/com/basic/array/SumZzero.java
@@ -0,0 +1,21 @@
+package com.basic.array;
+
+public class SumZzero {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        if (n % 2 == 0) {
+            for (int i = 0; i < n - 1; i = i + 2) {
+                res[i] = -(i + 1);
+                res[i + 1] = i + 1;
+            }
+        } else {
+            for (int i = 0; i < n - 1; i = i + 2) {
+                res[i] = i + 1;
+                res[i + 1] = -(i + 1);
+            }
+            res[n - 1] = 0;
+        }
+        return res;
+
+    }
+}
