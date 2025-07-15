29790a9289e9751d878827a1fb0b2cdaf1913ace
Sandeshcreator <mainalisandesh8@gmail.com>
2025-07-15 18:24:27 +0545

    (pattern8) :i + j == n + 1

--- src/basic/loop/Pattern8.java
+++ src/basic/loop/Pattern8.java
@@ -1,12 +1,12 @@
 package basic.loop;
 
-public class Pattern7 {
+public class Pattern8 {
     public static void main(String[] args) {
         int n = 5;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == j) {
+                if (i + j == n + 1) {
                     System.out.print("*\t");
                 } else {
                     System.out.print("\t");
