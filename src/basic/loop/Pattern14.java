09956d63b6c400d13a8b62e821efdb60bd1241d7
Sandeshcreator <mainalisandesh8@gmail.com>
2025-07-20 19:01:20 +0545

    loop

---
+++ src/basic/loop/Pattern14.java
@@ -0,0 +1,10 @@
+package basic.loop;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        int n = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+    }
+}
